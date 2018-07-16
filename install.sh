482957eefac263de4d2f8d4ab0b79d891f19f92b
Andrzej Hołowko <holowko@netguardians.ch>
2018-07-16 22:15:08 +0200

    initial commit

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+strfile -c % dev-oblique-strategies dev-oblique-strategies.dat
+sudo cp dev-oblique-strategies /usr/share/games/fortunes/
+sudo cp dev-oblique-strategies.dat /usr/share/games/fortunes/
+
